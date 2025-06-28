f8d4141b4cfd90337ac6237d1d69e1fd4908313d
SreekarSBS <sbssreekar@gmail.com>
2025-06-28 14:26:14 +0530

    regex , Dynamic routing, params,query

--- app.js
+++ app.js
@@ -2,39 +2,59 @@
 const express = require("express")
 // create a server
 const app = express()
+// app.use("/hello/api",(req,res) => {
+//     res.send("Hello Sreekar")
+// })
 
+// app.get("/user",(req,res) => {
+//     res.send("Got the data from the server")
+// })
+const ch = "*"
+app.get("/jo" + ch + "d",(req,res) => {
+    res.send("Jod bey")
+})
 
-app.use("/hello/api",(req,res) => {
-    res.send("Hello Sreekar")
+app.get("/user/:userId/:userName/:password",(req,res) => {
+    console.log(req.params);
+    
+    res.send("head travis or what")
+})
+
+app.get("/*fly$/",(req,res) => {
+
+    
+    res.send("butter fly ")
 })
 
 app.get("/user",(req,res) => {
-    res.send("Got the data from the server")
+    console.log(req.query);
+    
+    res.send("head repeat or what")
 })
 
-app.post("/user",(req,res) => {
-    res.send({ firstName : "Sreekar" , lastName : "SBS" })
-})
+// app.post("/user",(req,res) => {
+//     res.send({ firstName : "Sreekar" , lastName : "SBS" })
+// })
 
-app.put("/user",(req,res) => {
-    res.send({firstName : "Sanakkayala"})
-})
+// app.put("/user",(req,res) => {
+//     res.send({firstName : "Sanakkayala"})
+// })
 
-app.patch("/user",(req,res) => {
-    res.send({lastName:"Bhaskara Sai"})
-})
+// app.patch("/user",(req,res) => {
+//     res.send({lastName:"Bhaskara Sai"})
+// })
 
-app.delete("/user",(req,res) => {
-    res.send("We dont do that here.")
-})
+// app.delete("/user",(req,res) => {
+//     res.send("We dont do that here.")
+// })
 
-app.use("/hello/hello", (req,res) => {
-    res.send("Hello from the home page")
-})
+// app.use("/hello/hello", (req,res) => {
+//     res.send("Hello from the home page")
+// })
 
-app.use("/test", (req,res) => {
-    res.send("Hello from the test route handler")
-})
+// app.use("/test", (req,res) => {
+//     res.send("Hello from the test route handler")
+// })
 
 
 
