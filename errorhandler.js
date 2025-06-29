a31dc84aae1056a86a10de167a752f9b93964b07
SreekarSBS <sbssreekar@gmail.com>
2025-06-29 12:51:24 +0530

    playing with array of route handlers

---
+++ errorhandler.js
@@ -0,0 +1,22 @@
+const express = require("express")
+
+const app = express()
+
+app.put("/user106",
+    [(req,res,next) => {
+     console.log("Route handler 1");
+     next()
+      res.send("DevMeet is a Platform for developers to meet and connect with fellow developers online.")
+    
+    
+},[(req,res,next) => {
+    console.log("Im not sending any request , im just logging this");
+   next()
+}],(req,res,next) => {
+   console.log("Im just a human after all");
+    
+}])
+
+app.listen(9999,() =>{
+    console.log("Listening on port 9999");
+})
