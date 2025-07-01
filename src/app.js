28fd65523bb965e75d14d99d310595b002fff588
SreekarSBS <sbssreekar@gmail.com>
2025-07-01 12:36:55 +0530

    Pushing the data into MongoDB from Postman using express.json middleware

--- src/app.js
+++ src/app.js
@@ -5,15 +5,12 @@ const app = express()
 const ConnectDB = require("./config/database")
 const User = require("./models/user")
 
+// We want to fetch the data from the api (POST) through Postman , we need to parse it to JS Object
+app.use(express.json())
 
 app.post("/signup" , async(req , res) => {
-    const users = new User({
-    firstName :  "Rick",
-    lastName : "Grimes",
-    emailId : "Rick@email",
-    password : "GrimesUI",
-    gender : "M"
-})
+     const users = new User(req.body)
+    
 try {
 await users.save()
 res.send("User added successfully !")
